4ad9303f1b9117d6a9a495a92a273143f0315764
lintong-kooboo <lintong@kooboo.net>
2019-01-28 11:23:11 +0800

    add validation and classification module

--- src/components/register.vue
+++ src/components/register.vue
@@ -5,15 +5,19 @@
         <input type="file">
       </div>
       <div class="register-username">
-        <input type="text" placeholder="用户名">
+        <input type="text" @blur="showNameEM()" v-model="userName" placeholder="用户名">
+        <label class="error-message" v-if="errorMsg.nullName">*用户名不能为空</label>
       </div>
       <div class="register-password">
-        <input type="password" placeholder="密码">
+        <input type="password" @blur="showPwEM()" v-model="userPassword" placeholder="密码">
+        <label class="error-message display-error-message" v-if="errorMsg.nullPassword">*密码不能为空</label>
+        <label class="error-message display-error-message" v-if="errorMsg.errorPwFormat">*密码不能少于六位字符</label>
       </div>
       <div class="register-repassword">
-        <input type="password" placeholder="重复密码">
+        <input type="password" @blur="showRePwEM()" v-model="userRePassword" placeholder="重复密码">
+        <label class="error-message display-error-message" v-if="errorMsg.errorRePw">*两次输入的密码不一致</label>
       </div>
-      <input class="register-button" type="submit" value="注册">
+      <input class="register-button" v-on:click="goHomePage()" type="submit" value="注册">
     </form>
   </div>
 </template>
@@ -26,6 +30,63 @@ export default {
     isregister: {
       type: Boolean,
       default: false
+    }
+  },
+  data () {
+    return {
+      userName: '',
+      userPassword: '',
+      userRePassword: '',
+      errorMsg: {
+        nullName: false,
+        nullPassword: false,
+        errorPwFormat: false,
+        errorRePw: false
+      }
+    }
+  },
+  computed: {
+    nullName () {
+      return !this.userName
+    },
+    nullPassword () {
+      return !this.userPassword
+    },
+    errorPwFormat () {
+      return this.userPassword && this.userPassword.length < 6
+    },
+    errorRePw () {
+      return this.userPassword !== this.userRePassword
+    },
+    isInfoCorrect () {
+      return !this.nullName && !this.nullPassword && !this.errorPwFormat && !this.errorPwFormat && !this.errorRePw
+    }
+  },
+  methods: {
+    showNameEM () {
+      if (this.nullName) {
+        this.errorMsg.nullName = true
+      }
+    },
+    showPwEM () {
+      if (this.nullPassword) {
+        this.errorMsg.nullPassword = true
+      } else if (this.errorPwFormat) {
+        this.errorMsg.errorPwFormat = true
+      }
+    },
+    showRePwEM () {
+      if (this.errorRePw) {
+        this.errorMsg.errorRePw = true
+      }
+    },
+    goHomePage () {
+      if (this.isInfoCorrect) {
+        this.errorMsg = false
+        this.$router.push({
+          path: '/'
+        })
+      }
     }
   }
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
+  <div class="index-page">
     <b-nav-bar></b-nav-bar>
     <div class="blog">
       <b-posts></b-posts>
     </div>
+    <div class="classification">
+      <b-classification></b-classification>
+    </div>
+    <div class="blank"></div>
   </div>
 </template>
 
@@ -11,6 +15,7 @@
 import '../assets/scss/_index.scss'
 import NavBar from '../components/navBar'
 import Posts from '../components/posts'
+import Classification from '../components/classification'
 export default {
   name: 'posts',
   data () {
@@ -20,7 +25,8 @@ export default {
   },
   components: {
     'b-nav-bar': NavBar,
-    'b-posts': Posts
+    'b-posts': Posts,
+    'b-classification': Classification
   }
 }
 </script>
